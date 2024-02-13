93a9eb5f63c4a6545074dc9729bf2ab5f1b04a26
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2024-02-13 12:08:45 +0100

    Start on Responsive & Database

---
+++ assets/data/productData.php
@@ -0,0 +1,27 @@
+<?php
+
+$producten = [
+
+    ["product" => "Stone Cold Killer", "type" => "Dusttale Original", "price" => 10.99],
+
+    ["product" => "V.I.P.", "type" => "Dreamtale Original", "price" => 17.99],
+
+    ["product" => "Disbelief", "type" => "Undertale Remix", "price" => 14.99],
+
+    ["product" => "Infamous", "type" => "Dusttale Original", "price" => 16.99],
+
+    ["product" => "Dark Darker Yet Monster", "type" => "Undertale Remix", "price" => 9.99],
+
+    ["product" => "Tokyovania[Sans & Papyrus Vocals] ", "type" => "Undertale Remix", "price" => 14.99],
+
+    ["product" => "Megalotrousle", "type" => "Undertale Remix", "price" => 9.99],
+
+    ["product" => "Darker yet Darker", "type" => "Undertale Remix", "price" => 9.99],
+    
+    ["product" => "Battle Against a True Hero", "type" => "Undertale Remix", "price" => 9.99],
+    
+];
+
+echo json_encode($producten);
+
+?>
