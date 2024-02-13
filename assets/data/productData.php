20f3612d22127939847a63dc6ce8285588c6d898
MoonyDewDrops <julia.brouwervanoudshoorn@gmail.com>
2024-02-13 13:45:07 +0100

    More for Database & start on displaying  more products

--- assets/data/productData.php
+++ assets/data/productData.php
@@ -2,23 +2,50 @@
 
 $producten = [
 
-    ["product" => "Stone Cold Killer", "type" => "Dusttale Original", "price" => 10.99],
+    ["product" => "Stone Cold Killer", 
+    "type" => "Dusttale Original", 
+    "price" => 10.99, 
+    "Image" => "https://i.scdn.co/image/ab67616d0000b27337f7b15671c5778cf0a24408"],
 
-    ["product" => "V.I.P.", "type" => "Dreamtale Original", "price" => 17.99],
+    ["product" => "V.I.P.", 
+    "type" => "Dreamtale Original", 
+    "price" => 17.99, 
+    "Image" => "https://i.scdn.co/image/ab67616d0000b2735eeb8bef2852830aca370ecc"],
 
-    ["product" => "Disbelief", "type" => "Undertale Remix", "price" => 14.99],
+    ["product" => "Disbelief", 
+    "type" => "Undertale Remix", 
+    "price" => 14.99, 
+    "Image" => "https://i.scdn.co/image/ab67616d0000b273edc69ca99ecaa858859916d6"],
 
-    ["product" => "Infamous", "type" => "Dusttale Original", "price" => 16.99],
+    ["product" => "Infamous", 
+    "type" => "Dusttale Original", 
+    "price" => 16.99, 
+    "Image" => "https://i.scdn.co/image/ab67616d0000b27393e217d47ab46afb24e2add4"],
 
-    ["product" => "Dark Darker Yet Monster", "type" => "Undertale Remix", "price" => 9.99],
+    ["product" => "Dark Darker Yet Monster", 
+    "type" => "Undertale Remix", 
+    "price" => 9.99, 
+    "Image" => "https://i.scdn.co/image/ab67616d0000b2734bd80a0cd131f21debd0d07d"],
 
-    ["product" => "Tokyovania[Sans & Papyrus Vocals] ", "type" => "Undertale Remix", "price" => 14.99],
+    ["product" => "Tokyovania[Sans & Papyrus Vocals] ", 
+    "type" => "Undertale Remix", 
+    "price" => 14.99, 
+    "Image" => "https://i.scdn.co/image/ab67616d0000b27363bbb4ed7019cd5d3c515d08"],
 
-    ["product" => "Megalotrousle", "type" => "Undertale Remix", "price" => 9.99],
+    ["product" => "Megalotrousle", 
+    "type" => "Undertale Remix", 
+    "price" => 9.99, 
+    "Image" => "https://i.scdn.co/image/ab67616d0000b273d887b8e4146632d47ab0c6ee"],
 
-    ["product" => "Darker yet Darker", "type" => "Undertale Remix", "price" => 9.99],
+    ["product" => "Darker yet Darker", 
+    "type" => "Undertale Remix", 
+    "price" => 9.99, 
+    "Image" => "https://i.scdn.co/image/ab67616d0000b273b46c6f2180b6a41487fed6d0"],
     
-    ["product" => "Battle Against a True Hero", "type" => "Undertale Remix", "price" => 9.99],
+    ["product" => "Battle Against a True Hero", 
+    "type" => "Undertale Remix", 
+    "price" => 9.99, 
+    "Image" => "https://i.scdn.co/image/ab67616d0000b2734ae9002283502758b498d615"],
     
 ];
 
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,23 @@
+
+
+
+async function getProductData(file) {
+  const response = await fetch(file);
+  const data = await response.json();
+  setRadarChart(data);
+}
+
+
+getProductData('./assets/data/productData.php');
+
+
+function setProduct(data) {
+  const names = data.map((object) => object.product);
+  const types = data.map((object) => object.type);
+  const prices = data.map((object) => object.price);
+  const url = data.map((object) => object.Image);
+}
+
 var slider = document.getElementById("myRange");
 var output = document.getElementById("prijs");
 output.innerHTML = slider.value;
